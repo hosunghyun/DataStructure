82147e5df352f344ea42705650045e2e41bd15ce
hyunhosung <hhs04321@naver.com>
2024-08-29 20:37:20 +0900

    sequencestack/0829

--- STACK/SequenceStack.c
+++ STACK/SequenceStack.c
@@ -7,35 +7,35 @@ int top = 0;
 
 // 스택이 공백인지 확인
 int isEmpty() {
-    if (top == 0) return 1;
+    if (top == 0) return 1;     // top이 0이므로 현재 스택은 공백상태
     else return 0;
 }
 
 // 스택이 가득찼는지 확인
 int isFull() {
-    if (top == STACK_SIZE) return 1;
+    if (top == STACK_SIZE) return 1;    // top이 스택의 크기랑 같으면 가득찬 상태이다.
     else return 0;
 }
 
 // 스택에 삽입
 void push(int data) {
-    if (isFull() == 1) {
+    if (isFull() == 1) {    // 가득찼기 때문에 스택에 값을 넣을 수 없다.
         printf("STACK is FULL\n");
         return;
     }
     else {
-        STACK[top++] = data;
+        STACK[top++] = data;    // 스택에 값을 넣을 수 있기 때문에 top에 값을 넣고 top을 1 증가 시킨다.
     }
 }
 
 // 스택에 삭제
 int pop() {
-    if (isEmpty() == 1) {
+    if (isEmpty() == 1) {       // 스택이 비었기 때문에 값을 반환할 수 없다.
         printf("STACK is Empty\n");
         return 0;
     }
     else {
-        return STACK[--top];
+        return STACK[--top];    // top이 값을 가리키기 위해 1을 감소시키고 출력
     }
 }
 
--- STACK/SequenceStack.java
+++ STACK/SequenceStack.java
@@ -1,35 +1,35 @@
 package STACK;
 
 class Stack {
-    public final int STACK_SIZE = 100;
-    public int[] stack = new int[STACK_SIZE];
-    public int top = 0;
+    public final int STACK_SIZE = 100;      // 스택의 최대 크기를 정해준다.
+    public int[] stack = new int[STACK_SIZE];   // 스택의 최대 크기만큼 배열을 생성한다.
+    public int top = 0;     // top을 설정한다.
 
-    public boolean isEmpty() {
-        if(top == 0) {
+    public boolean isEmpty() {      // 스택이 비었는지 확인
+        if(top == 0) {      // top이 0이기 때문에 스택은 비었다.
             return true;
         }
         return false;
     }
-    public boolean isFull() {
-        if(top == STACK_SIZE) {
+    public boolean isFull() {   // 스택이 가득 찼는지 확인
+        if(top == STACK_SIZE) {     // top이 스택의 크기와 같기 때문에 가득 찼다.
             return true;
         }
         return false;
     }
     public void push(int data) {
-        if(isFull()) {
+        if(isFull()) {      // 가득 찼으면 true가 반환 되기에 중괄호 실행
             System.out.println("Stack is Full!");
             return;
         }
-        stack[top++] = data;
+        stack[top++] = data;    // 스택 top 위치에 data를 넣고 top은 1 증가
     }
     public int pop() {
-        if(isEmpty()) {
+        if(isEmpty()) {     // 스택이 비었으면 true를 반홚여 중괄호 실행
             System.out.println("Stack is Empty!");
             return -1;
         }
-        return stack[--top];
+        return stack[--top];    // 스택의 top 위치에 값을 반환 top은 1 감소
     }
 }
 
